6d854f1405d10a8a23994b4a2afdb30388b2120b
adrbalcazar144inf-art <adrbalcazar144.inf@industrialmurillo.edu.bo>
2026-06-27 18:03:18 -0400

    Delete htdocs/lib directory

--- htdocs/lib/webauthn-framework-5.4.x/src/stimulus/assets/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as AuthenticationController } from "./authentication-controller.js";
-export { default as RegistrationController } from "./registration-controller.js";
-export { default as WebauthnController } from "./controller.js";
-export { default as BaseController } from "./base-controller.js";
